c6a83314a03d0a982a839bdbeff8fe0371a60878
dirane <diranetafen@yahoo.com>
2020-02-20 22:57:10 +0000

    add dockerhub build autotest

---
+++ simple_api/run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+if [ "$(curl -u toto:python -X GET http://127.0.0.1:5000/pozos/api/v1.0/get_student_ages | python -c "import sys, json; print json.load(sys.stdin)['student_ages']['bob']")" = "13" ] && [ "$(curl -u toto:python -X GET http://127.0.0.1:5000/pozos/api/v1.0/get_student_ages | python -c "import sys, json; print json.load(sys.stdin)['student_ages']['alice']")" = "12" ]; then echo "test OK"; exit 0; else echo "test KO"; exit 1; fi
